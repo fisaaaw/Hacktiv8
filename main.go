76369d548212eea025b0f6562c974622bf3db977
ziyadatun nur afisa <ziyadatun.afisa@mncbank.co.id>
2022-08-22 20:16:41 +0700

    slice

--- main.go
+++ main.go
@@ -8,8 +8,13 @@ func main() {
 	for i := 0; i <=10; i++ {
 		if i % 2 == 1 {
 			fmt.Println(i, "ganjil")
-		} else if i % 2  == 0 {
+		} else {
 			fmt.Println(i, "genap")
 		}
+	}
+
+	name := []string{"teguh", "fadli", "rafi", "david", "fajar", "san", "kadek", "stevanus", "iqbal", "kevin"}
+	for _, a := range name {
+	fmt.Println(a)
 	}
 }

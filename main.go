093228b486b6e0fa25c944540e8cadf2b50ecfd5
ziyadatun nur afisa <ziyadatun.afisa@mncbank.co.id>
2022-08-30 19:16:09 +0700

    latihan

--- main.go
+++ main.go
@@ -1,31 +1,49 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type namaTemen struct {
-	name string
+func print(names ...string) []map[string]string {
+	var result []map[string]string
+
+	for i, v := range names {
+		key := fmt.Sprintf("student%d", i+1)
+		temp := map[string]string{
+			key: v,
+		}
+		result = append(result, temp)
+	}
+	return result
+}
+
+func summ(number ...int) (out map[string]int, kalimat string) {
+	total := 0
+
+	for _, v := range number {
+		total += v
+	}
+
+	out = map[string]int{
+		"total": total,
+	}
+	
+	return
+}
+
+func sum(numbers ...int) (total int) {
+	total = 0
+
+	for _, v := range numbers {
+		total += v
+	}
+	return 
 }
 
 func main() {
-	var friends []*namaTemen
-	name1 := []string{"anton", "putra", "david", "rafi", "fadli", "putri"}
 
-	for _, a := range name1 {
-			v := namaTemen{
-			name: a,
-			}
-			friends = append(friends, &v)
+	numberList := sum(1, 2, 3, 4, 5, 6, 7, 8)
 
-	}
+	//numberList := []int{1,2,3,4,5,6,7,8}
+	//result, kalimat := summ(1, 2, 3, 4, 5, 6, 7, 8)
 
-	print := func(friends []*namaTemen) {
-		for _, fullNames := range friends {
-			fmt.Println(*&fullNames.name)
-		}
-
-	}
-
-	print(friends)
+	fmt.Println("Result:", numberList)
 }
